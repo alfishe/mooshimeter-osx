a55f361ea4db16157a53da6ad1b2783e648ce4af
Ilia Sharin <isharin@gmail.com>
2016-08-27 14:15:57 -0400

    Initial Commit

---
+++ mooshimeter-osx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mooshimeter-osx
+//
+//  Created by Dev on 8/27/16.
+//  Copyright © 2016 alfishe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ mooshimeter-osx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mooshimeter-osx
+//
+//  Created by Dev on 8/27/16.
+//  Copyright © 2016 alfishe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
